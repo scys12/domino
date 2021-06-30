a60026f1aa55779b6bf968161e5bca4ea67601f1
Ammar Alifian Fahdan <kerupuksambel.2000@gmail.com>
2021-06-30 11:24:00 +0700

    Renaming

--- room/server.py
+++ room/server.py
@@ -1,79 +1,92 @@
 import socket
 import select
-import queue
-from threading import Thread
-from time import sleep
-# from random import randint
 import sys
-import pickle
+import threading
+import pickle, base64
+from random import randint
 
-class ProcessThread(Thread):
-	def __init__(self):
-		super(ProcessThread, self).__init__()
-		self.running = True
-		self.q = queue.Queue()
 
-	def add(self, data):
-		self.q.put(data)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-	def stop(self):
-		self.running = False
+ip_address = 'localhost'
+port = 8081
+server.bind((ip_address, port))
+server.listen(100)
+waiting_room = []
+rooms = {}
 
-	def run(self):
-		q = self.q
-		while self.running:
-			try:
-				# block for 1 second only:
-				value = q.get(block=True, timeout=1)
-				print(value)
-				process(value)
-			except queue.Empty:
-				sys.stdout.write('.')
-				sys.stdout.flush()
+def serialize(msg):
+	return msg.encode()
 
-		if not q.empty():
-			print("Elements left in the queue:")
-			while not q.empty():
-				print(q.get())
-
-t = ProcessThread()
-t.start()
-
-def process(value):
-	"""Implement this. Do something useful with the received data."""
-	print(value)
-
-	# sleep(randint(1,5))    # emulating processing time
-
-def main():
-	s = socket.socket()                # Create a socket object
-	host = 'localhost' # Get local machine name
-	port = 5001                      # Reserve a port for your service.
-	s.bind((host, port))                 # Bind to the port
-	print("Listening on port {p}...".format(p=port))
-	s.listen(5)                 # Now wait for client connection.
+def clientthread(conn, addr):
 	while True:
 		try:
-			client, addr = s.accept()
-			print(client, addr)
-			ready = select.select([client,],[], [],2)
-			if ready[0]:
-				data = client.recv(4096)
-				print(data)
-				t.add(data)
-		except KeyboardInterrupt:
-			print("Stop.")
-			break
-		except socket.error as msg:
-			print("Socket error! %s" % str(msg))
-			break
-		cleanup()
-	s.close()
+			msg = conn.recv(2048)
+			if msg:
+				print('Data recv : {}'.format(msg))
+				broadcast_waiting_room(msg)
+			else:
+				remove(conn)
+		except Exception as e:
+			print(e)
+			continue
 
-def cleanup():
-	t.stop()
-	t.join()
+def private(msg, dst):
+	# print(dst)
+	try:
+		dst.send(msg)
+	except Exception as e:
+		print(e)
+		remove(dst)
+		dst.close()
 
-if __name__ == '__main__':
-	main()
+def broadcast_waiting_room(msg):
+	print('Broadcasting ' + msg.decode())
+	for c in waiting_room:
+		if c:
+			print(c)
+			try:
+				c.send(msg)
+				print('Message sent to {}!'.format(str(c)))
+			except Exception as e:
+				print(e)
+				remove(c)
+				c.close()
 
+def broadcast_room(msg, room):
+	print('Broadcasting ' + msg.decode())
+	for c in room:
+		if c:
+			print(c)
+			try:
+				c.send(msg)
+				print('Message sent to {}!'.format(str(c)))
+			except Exception as e:
+				print(e)
+				remove(c)
+				c.close()
+
+def remove(conn):
+	for r in rooms:
+		if conn in rooms[r]:
+			rooms[r].remove(conn)
+
+while True:
+	conn, addr = server.accept()
+	waiting_room.append(conn)
+	if(len(waiting_room) == 1):
+		for l in waiting_room:
+			private(serialize('Silakan menunggu'), l)
+	elif(len(waiting_room) == 2):
+		id_room = str(randint(1000, 9999))
+		while id_room in rooms:
+			id_room = str(randint(1000, 9999))
+		rooms[id_room] = waiting_room
+		for l in waiting_room:
+			private(serialize(f'Anda sudah terpasangkan. ID Room Anda adalah {id_room}'), l)
+		waiting_room = []
+		print(rooms)
+	print(':'.join([str(_) for _ in addr]) + ' connected')
+	threading.Thread(target = clientthread, args=(conn, addr)).start()
+conn.close()
