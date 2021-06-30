35505f5e06e59ce442ec82b742562d3e6f2ce054
syubban <syubbanfakhriya@gmail.com>
2021-06-30 16:07:03 +0700

    generate random card

---
+++ gameplay/app.py
@@ -0,0 +1,10 @@
+from board import Board
+
+
+if __name__ == "__main__":
+  board = Board()
+  board.generate_card("")
+
+  print("player1 card", board.player1_cards)
+  print("player2 card", board.player2_cards)
+  print("first card", board.firstCard)
