6d72909a50e6471eb59006ce71c8bc40a7df23ba
Adrian Danindra <60964359+Bluejake3@users.noreply.github.com>
2021-06-30 23:26:41 +0700

    1.1: Add waiting room

--- Main Menu/mainmenu.py
+++ Main Menu/mainmenu.py
@@ -2,20 +2,27 @@ import pygame_menu
 from pygame_menu.examples import create_example_window
 
 from typing import Tuple, Any
-
 surface = create_example_window('Simple Domino', (640, 360))
 mytheme = pygame_menu.themes.THEME_GREEN.copy()
 mytheme.title_font=pygame_menu.font.FONT_FRANCHISE
 mytheme.widget_font=pygame_menu.font.FONT_BEBAS
-def start_the_game() -> None:
-    """
-    Function that starts a game. This is raised by the menu button,
-    here menu can be disabled, etc.
-    :return: None
-    """
+
+def start_the_game():
     global user_name
     print('{0}, Do the job here!'.format(user_name.get_value()))
+    menu.remove_widget("Play")
+    menu.remove_widget("quit")
+    menu.add.button("Return", Refresh, button_id='Return')
+    menu.add.button('Quit', pygame_menu.events.EXIT)
+    menu.add.label(wait, max_char=40, font_size=20)
 
+def Refresh():
+    menu.clear()
+    image= "./logo.png"
+    menu.add.image(image, angle=0, scale=(0.15, 0.15))
+    user_name = menu.add.text_input('Name: ', default='Player 1', maxchar=10)
+    play_button = menu.add.button('Play', start_the_game,button_id='Play')
+    menu.add.button('Quit', pygame_menu.events.EXIT, button_id="quit")
 
 menu = pygame_menu.Menu(
     height=360,
@@ -23,11 +30,14 @@ menu = pygame_menu.Menu(
     title='Main Menu',
     width=640
 )
+
+wait = "Waiting for other player to join"
+
 image= "./logo.png"
 menu.add.image(image, angle=0, scale=(0.15, 0.15))
 user_name = menu.add.text_input('Name: ', default='Player 1', maxchar=10)
-menu.add.button('Play', start_the_game)
-menu.add.button('Quit', pygame_menu.events.EXIT)
+play_button = menu.add.button('Play',start_the_game,button_id='Play')
+menu.add.button('Quit', pygame_menu.events.EXIT, button_id='quit')
 
 if __name__ == '__main__':
     menu.mainloop(surface)
